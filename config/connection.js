cbaf59e31bb6a3b1738fed6501084a3986a44632
zanyssii <ataminnka@gmail.com>
2019-10-24 20:14:41 -0600

    connected to db

--- config/connection.js
+++ config/connection.js
@@ -4,18 +4,18 @@ var keys = require("./keys.js");
 var mysql = require("mysql");
 var connection;
 
-// if (process.env.JAWSDB_URL) {
-//   connection = mysql.createConnection(process.env.JAWSDB_URL);
-// }
-// else {  
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+else {  
   connection = mysql.createConnection({
-    // host: "localhost",
+    host: "localhost",
     port: 3306,
     user: "root",
     password: "",
     database: "burgers_db" 
   });
-//}
+}
 
 connection.connect(function(err) {
   if (err) {
--- server.js
+++ server.js
@@ -18,7 +18,5 @@ var routes = require("./controllers/burgers_controller.js");
 
 app.use(routes);
 app.listen(PORT, function() {
-
-
   console.log("Server listening on: http://localhost:" + PORT);
 });
