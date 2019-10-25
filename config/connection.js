40a4388cc9f32cbf76f43fadf1ba6ab828eb9627
zanyssii <ataminnka@gmail.com>
2019-10-24 20:17:19 -0600

    wip

--- config/connection.js
+++ config/connection.js
@@ -6,8 +6,7 @@ var connection;
 
 if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
-}
-else {  
+} else {  
   connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
